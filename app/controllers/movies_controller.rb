c8b34cf9cd9a78ecbfafc7d6241fe9bcc561584a
Waine Tam <wainetam@gmail.com>
2013-10-26 15:37:43 -0400

    add session hash

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  # session: on # creates session hash
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -6,29 +8,46 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = all_ratings()
+    # session.clear
+    unless params[:sort]
+      session[:sort] ? params[:sort] = session[:sort] : @movies = Movie.all
+    end
+
+      @all_ratings = all_ratings()
     if params[:sort] == 'title'
       @movies = Movie.order('title ASC')
       @hilite_title = true
+      # session.delete[:sort]
+      session[:sort] = 'title'
     elsif params[:sort] == 'release_date'
       @movies = Movie.order('release_date DESC')
       @hilite_date = true
+      # session.delete[:sort]
+      session[:sort] = 'release_date'
     else
+      # session[:sort] ? params[:sort] = session[:sort] : @movies = Movie.all
       @movies = Movie.all
+      # session[:filtered_movies] = @movies
     end
 
+    unless params[:ratings]
+      session[:ratings] ? params[:ratings] = session[:ratings] : @movies = Movie.all
+    end
     # to save selected boxes
     if params[:ratings] == nil # upon first visit to the site
-      @ratings_filter = ['G', 'NC-17', 'PG', 'PG-13', 'R']
+      @ratings_filter = ['G', 'NC-17', 'PG', 'PG-13', 'R'] # shows all checkboxes checked
+      # session[:ratings] = nil
     else
       selected_movies = []
+      session[:ratings] = params[:ratings]
       @ratings_filter = params[:ratings].keys
       @ratings_filter.each do |rating|
         selected_movies << Movie.where("rating = '#{rating}'")
         selected_movies.flatten!
       end
       @movies = selected_movies
-    end 
+    end
+    # session[:filtered_movies] = @movies
   end
 
   def new
